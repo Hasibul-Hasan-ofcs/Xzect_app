c02c0ae121d7bd67be08afddacf85112fa1e1c1e
MD-Hasibul_Hasan <hasibulofcs@gmail.com>
2023-08-28 00:11:39 +0600

    route issue fixing

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 "use client";
+import { useEffect } from "react";
 import styles from "./page.module.css";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-  router.push("/course");
+  useEffect(() => {
+    const router = useRouter();
+    router.push("/course");
+  }, []);
 
   return <main className={styles.main}></main>;
 }
